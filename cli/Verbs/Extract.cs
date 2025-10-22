087b0c1da03ecac8d4908e91c4cf03910a1cb1b8
Manuel Naujoks <mnaujoks@gmail.com>
2025-10-22 20:44:20 +0200

    creditcardstatement extraction started

---
+++ cli/Verbs/Extract.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using Microsoft.Extensions.Logging;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace cli.Verbs
+{
+	[Verb("extract")]
+	class Extract
+	{
+		[Value(0, Required = true)]
+		public string FilePath { get; set; } = null!;
+
+		public async Task Do(ILogger<Extract> logger)
+		{
+			var file = new FileInfo(FilePath!);
+			using var fileStream = file.OpenRead();
+
+			using var http = new HttpClient();
+			using var response = await http.PostAsync(
+				requestUri: "https://localhost:7026/extract",
+				content: new MultipartFormDataContent
+				{
+					{ new StreamContent(fileStream), "file", file.Name.ToLower() }
+				});
+
+			response.EnsureSuccessStatusCode();
+			var content = await response.Content.ReadFromJsonAsync<JsonElement>();
+			Json.Out(content);
+		}
+	}
+}
--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,9 @@
+using AgentDo;
+using AgentDo.Content;
+using AgentDo.OpenAI;
 using Microsoft.AspNetCore.SignalR;
+using OpenAI.Chat;
+using System.ComponentModel;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,20 +14,30 @@ builder.Services.AddSignalR(o =>
 });
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("", policy =>
-    {
-        policy.WithOrigins("https://localhost:3000"/*Excel Addin*/)
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
+	options.AddPolicy("", policy =>
+	{
+		policy.WithOrigins("https://localhost:3000"/*Excel Addin*/)
+			  .AllowAnyHeader()
+			  .AllowAnyMethod()
+			  .AllowCredentials();
+	});
+});
+
+builder.Services.AddSingleton(sp => new ChatClient(
+	model: "gpt-4o",
+	apiKey: builder.Configuration["OPENAI_API_KEY"]!));
+
+builder.Services.AddSingleton<IAgent, OpenAIAgent>();
+builder.Services.Configure<OpenAIAgentOptions>(o =>
+{
+	o.Temperature = 0.0f;
 });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.MapOpenApi();
 }
 
 app.UseCors("");
@@ -31,7 +46,35 @@ app.UseHttpsRedirection();
 
 app.MapGet("/hello", () =>
 {
-    return Results.Ok(new { text = "Hello World!" });
+	return Results.Ok(new { text = "Hello World!" });
+});
+
+app.MapPost("/extract", async (HttpRequest request, IAgent agent) =>
+{
+	if (!request.HasFormContentType || request.Form.Files.Count == 0)
+	{
+		return Results.BadRequest(new { error = "No file uploaded." });
+	}
+
+	using var memoryStream = new MemoryStream();
+	var file = request.Form.Files[0];
+	await file.CopyToAsync(memoryStream);
+
+	using var document = Document.From(memoryStream, file.FileName);
+	CreditCardStatement? creditCardStatement = default;
+	var messages = await agent.Do(//todo use agent.Get<>()
+		task: new Prompt("Here is my credit card statement.", document),
+		tools:
+		[
+			Tool.From(toolName: "CreditCardStatement", tool: [Description("Understand the credit card statement.")]
+			(CreditCardStatement c, Tool.Context context) =>
+			{
+				context.Cancelled = true;
+				creditCardStatement = c;
+			}),
+		]);
+
+	return Results.Ok(creditCardStatement);
 });
 
 app.MapHub<Browser2ExcelHub>("/hub");
@@ -88,3 +131,6 @@ public class Browser2ExcelHub : Hub
 
 	static string Truncated(string value, int maxLength = 500) => value.Length <= maxLength ? value : value[..maxLength] + "...";
 }
+
+record CreditCardStatement(DateTime Start, DateTime End, string Number, Booking[] Bookings, [property: Description("Pay attention if its positive or negative.")] Amount NewSaldo);
+record Booking(DateTime BelegDatum, DateTime BuchungsDatum, string Zweck, Amount BetragInEuro, string? Waehrung = null, Amount? Betrag = null, string? Kurs = null, Amount? WaehrungsumrechnungInEuro = null);
